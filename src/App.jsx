6bd85b994412bf361512a323ac3a48a7c81af0fe
TalibIbrahim <talibibrahim04@gmail.com>
2025-05-19 16:06:35 +0500

    Setup React + Vite frontend basics

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Quickdrop</h1>
+      <p>Fast file sharing with no login.</p>
+    </div>
+  );
+}
+
+export default App;
